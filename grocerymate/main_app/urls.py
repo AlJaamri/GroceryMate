20b1878a180049ee8c7737a7c66682a98a8e86c5
AlJaamri <husain.aljamri@gmail.com>
2026-02-09 18:25:20 +0300

    added sign up path in urls.py

--- grocerymate/main_app/urls.py
+++ grocerymate/main_app/urls.py
@@ -12,10 +12,12 @@ urlpatterns = [
     path('groceries/<int:grocery_id>/add_bought/', views.add_bought, name='add_bought'),
     path('groceries/<int:grocery_id>/apply_coupon/<int:coupon_id>/', views.apply_coupon, name='apply_coupon'),
     path('groceries/<int:grocery_id>/unapply_coupon/<int:coupon_id>/', views.unapply_coupon, name='unapply_coupon'),
-    
+
     path('coupons/', views.CouponList.as_view(), name='coupons_index'),
     path('coupons/<int:pk>/', views.CouponDetail.as_view(), name='coupons_detail'),
     path('coupons/create/', views.CouponCreate.as_view(), name='coupons_create'),
     path('coupons/<int:pk>/update/', views.CouponUpdate.as_view(), name='coupons_update'),
     path('coupons/<int:pk>/delete/', views.CouponDelete.as_view(), name='coupons_delete'),
+
+    path('accounts/signup/', views.signup, name='signup')
 ]
