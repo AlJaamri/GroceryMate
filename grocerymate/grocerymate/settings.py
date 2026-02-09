85c5d63ca2ddc6bf1da4479bd3f42f279dacc686
AlJaamri <husain.aljamri@gmail.com>
2026-02-09 18:52:14 +0300

    settings.py changes for .env

--- grocerymate/grocerymate/settings.py
+++ grocerymate/grocerymate/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +78,11 @@ WSGI_APPLICATION = 'grocerymate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'grocerymate',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('DATABASE_NAME'),
+        "USER": os.getenv('DATABASE_USER'),
+        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
+        "PORT": os.getenv('DATABASE_PORT'),
     }
 }
 
