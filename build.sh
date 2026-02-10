e4d940f21f5ec240f1887cfac8bdf30421754468
AlJaamri <husain.aljamri@gmail.com>
2026-02-10 21:13:37 +0300

    more deployment progress

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Change directory into the Django project folder
+cd grocerymate
+
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Run migrations
 python manage.py migrate
