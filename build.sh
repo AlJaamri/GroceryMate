6b644dcd1db7f1d2a0d78d4b50fc03c115c8e0cf
AlJaamri <husain.aljamri@gmail.com>
2026-02-10 22:05:37 +0300

    L

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Change directory into the Django project folder
-cd grocerymate
-
-# Collect static files
 python manage.py collectstatic --no-input
 
-# Run migrations
 python manage.py migrate
-cd ..
