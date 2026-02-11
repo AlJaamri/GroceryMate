a7085d0494ba3a165c67efe1bc482728fbe00a0f
AlJaamri <husain.aljamri@gmail.com>
2026-02-11 20:21:23 +0300

    deployment progress

--- grocerymate/settings.py
+++ grocerymate/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%c80byghl!jsahv72^j@kya-(ccxm$f=lg1n@6r6__c$-+$58a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "grocerymate-tx81.onrender.com",
